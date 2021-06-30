15a7de6595e91e78a38ff2d1199266465c12a5ab
Nay Ferreira <=>
2021-06-29 22:50:16 -0300

    servico e modelo de ordem de servico

---
+++ src/app/models/Ordem-Servico.ts
@@ -0,0 +1,10 @@
+export interface Os {
+    id?: any;
+    created_at?: any,
+    closed_on?: any,
+    priority: any,
+    obeservations: String,
+    client: any,
+    status: any,
+    technique: any
+}
